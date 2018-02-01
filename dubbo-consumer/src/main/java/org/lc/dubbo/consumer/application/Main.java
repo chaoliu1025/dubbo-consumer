ccf9dcdc6cf541eebadb123f0d942a2e7953328e
chaoliu1025 <chaoliu1025@163.com>
2018-02-01 19:08:18 +0800

    add consumer for dubbo

---
+++ dubbo-consumer/src/main/java/org/lc/dubbo/consumer/application/Main.java
@@ -0,0 +1,15 @@
+package org.lc.dubbo.consumer.application;
+
+import org.lc.dubbo.service.inf.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
+        context.start();
+        DemoService demoService = (DemoService)context.getBean("demoService"); // 获取远程服务代理
+        String hello = demoService.sayHello("world"); // 执行远程方法
+        System.out.println( hello ); // 显示调用结果
+	}
+}
